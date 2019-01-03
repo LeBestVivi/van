0bc0b843d8cbf51605564d3a87d86dd90413fc5a
vanished <vanished.apps@gmail.com>
2019-01-03 09:16:07 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, msg, args) => {
-  msg.channel.send(bot.ping + ":ping_pong:")
+  msg.channel.send(client.ping + ":ping_pong:")
   }
 
 module.exports.help = {
