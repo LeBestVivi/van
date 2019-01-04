8c9b73591f4e8b7b4a2bc2ea05747af1f1fe3deb
CarRoy <43969981+CarRoy@users.noreply.github.com>
2019-01-04 16:44:41 +0100

    you didnt see anything

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,8 @@ module.exports.run = async (client, msg, args) => {
     .setDescription("Help")
     .setColor("#7289DA")
     .addField("Moderation Commands", "**warn** - `!warn @mention [reason]`\n**warnlevel** - `!warnlevel @mention`\n**tempmute** - `!tempmute @mention [time]`\n**kick** - `!kick @mention [reason]`\n**ban** - `!ban @mention [reason]`\n**addrole** - `!addrole @mention @role`\n**removerole** - `!removerole @mention @role`\n**report** - `!report @mention [reason]`")
-    .addField("Fun Commands", "**reddit** - `!reddit [subreddit]`\n**ping** - `!ping`\n**hug** - `!hug @mention`\n**reply** - `!reply`")
-    .addField("Bot Owner Only Commands", "**eval** - `!eval [eval]`\n**pull** - `!pull`\n**avatar** - `!avatar`")
+    .addField("Fun Commands", "**reddit** - `!reddit [subreddit]`\n**ping** - `!ping`\n**hug** - `!hug @mention`\n**reply** - `!reply`\n**avatar** - `!avatar`")
+    .addField("Bot Owner Only Commands", "**eval** - `!eval [eval]`\n**pull** - `!pull`")
     .setFooter("Van | Don't use [] in commands. | Requested by: " + msg.author.username);
 
   msg.channel.send(helpEmbed)
