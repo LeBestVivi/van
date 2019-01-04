986ae228935b962e3c2152fa90efc3de70a73cb0
CarRoy <43969981+CarRoy@users.noreply.github.com>
2019-01-04 16:25:37 +0100

    Added !avatar to help embed

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, msg, args) => {
     .setColor("#7289DA")
     .addField("Moderation Commands", "**warn** - `!warn @mention [reason]`\n**warnlevel** - `!warnlevel @mention`\n**tempmute** - `!tempmute @mention [time]`\n**kick** - `!kick @mention [reason]`\n**ban** - `!ban @mention [reason]`\n**addrole** - `!addrole @mention @role`\n**removerole** - `!removerole @mention @role`\n**report** - `!report @mention [reason]`")
     .addField("Fun Commands", "**reddit** - `!reddit [subreddit]`\n**ping** - `!ping`\n**hug** - `!hug @mention`\n**reply** - `!reply`")
-    .addField("Bot Owner Only Commands", "**eval** - `!eval [eval]`\n**pull** - `!pull`")
+    .addField("Bot Owner Only Commands", "**eval** - `!eval [eval]`\n**pull** - `!pull`\n**avatar** - `!avatar`")
     .setFooter("Van | Don't use [] in commands. | Requested by:" + msg.author.username);
 
   msg.channel.send(helpEmbed)
