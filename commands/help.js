98df49bed08f2bd0d30b8120d11a5aa0d593a931
vanished <ibrahimcajlakovic1@gmail.com>
2019-01-05 11:51:58 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, msg, args) => {
   let helpEmbed = new Discord.RichEmbed()
     .setDescription("Help")
     .setColor("#7289DA")
-    .addField("Moderation Commands", "**warn** - `!warn @mention [reason]`\n**warnlevel** - `!warnlevel @mention`\n**tempmute** - `!tempmute @mention [time]`\n**kick** - `!kick @mention [reason]`\n**ban** - `!ban @mention [reason]`\n**addrole** - `!addrole @mention @role`\n**removerole** - `!removerole @mention @role`\n**report** - `!report @mention [reason]`")
+    .addField("Moderation Commands", "**warn** - `!warn @mention [reason]`\n**warnlevel** - `!warnlevel @mention`\n**tempmute** - `!tempmute @mention [time]`\n**kick** - `!kick @mention [reason]`\n**ban** - `!ban @mention [reason]`\n**addrole** - `!addrole @mention @role`\n**removerole** - `!removerole @mention @role`\n**report** - `!report @mention [reason]`\n**clean** - `!clean [all|user] [int|id]`")
     .addField("Fun Commands", "**reddit** - `!reddit [subreddit]`\n**ping** - `!ping`\n**hug** - `!hug @mention`\n**reply** - `!reply`\n**avatar** - `!avatar`")
     .setFooter("Van | Don't use [] in commands. | Requested by: " + msg.author.username);
 
