b87f12a243ab1aed5e1ebedd50be733021d60c7d
Rage <38593512+Rage0001@users.noreply.github.com>
2019-01-05 16:03:31 +0200

    more fixes

--- commands/jumbo.js
+++ commands/jumbo.js
@@ -3,70 +3,72 @@ const twemoji = require("twemoji");
 const snek = require("snekfetch");
 const mg = require("merge-img");
 const resizeImg = require("resize-img");
+const moment = require("moment");
 
 const discordJumbo = async (emoji, type) => {
- let url = `https://cdn.discordapp.com/emojis/${emoji}.${type}`
- var { body: buffer } = await snek.get(`${url}`)
- if(type === "png") {
-  return {
-    buffer: await resizeImg(buffer, {width: 72, height: 72}),
-    type
-  }
- } else {
-  return {
-    buffer,
-    type
+  let url = `https://cdn.discordapp.com/emojis/${emoji}.${type}`
+  var { body: buffer } = await snek.get(`${url}`)
+  if(type === "png") {
+   return {
+     buffer: await resizeImg(buffer, {width: 72, height: 72}),
+     type
+   }
+  } else {
+   return {
+     buffer,
+     type
+   }
   }
  }
-}
+ 
+ const twemojiJumbo = async (emoji) => {
+     var twemote = twemoji.parse(emoji);
+     let twregex = /src="(.+)"/g;
+     var regtwemote = twregex.exec(twemote)[1];
+     var { body: buffer } = await snek.get(`${regtwemote}`);
+     return buffer
+ }
 
-const twemojiJumbo = async (emoji) => {
-    var twemote = twemoji.parse(emoji);
-    let twregex = /src="(.+)"/g;
-    var regtwemote = twregex.exec(twemote)[1];
-    var { body: buffer } = await snek.get(`${regtwemote}`);
-    return buffer
-}
-
-module.exports.run = async(client, msg, args) => {
-    if(args.length <= 1){
-      if(args[0].startsWith("<")){
-        let type
-        let id = args[0].split(":")[2].slice(0, -1)
-        if(args[0].split(":")[0].endsWith("a")) {
-          type = "gif"
-        } else {
-          type = "png"
-        }
-        let jumbo = await discordJumbo(id, type)
-        msg.channel.send(jumbo.buffer)
+module.exports.run = async (client, message, args) => {
+  if(args.length === 1){
+    if(args[0].startsWith("<")){
+      let type
+      let id = args[0].split(":")[2].slice(0, -1)
+      if(args[0].split(":")[0].endsWith("a")) {
+        type = "gif"
       } else {
-        let buffer = await twemojiJumbo(args[0])
-        msg.channel.send(buffer)
+        type = "png"
       }
+      let jumbo = await discordJumbo(id, type)
+      message.channel.send(jumbo.buffer)
     } else {
-      var buf = []
-    for (emoji of args) {
-      if(emoji.startsWith("<")) {
-            let id = emoji.split(":")[2].slice(0, -1)
-            let buffer = await discordJumbo(id, "png")
-            buf.push(buffer.buffer)
-          } else {
-            let buffer = await twemojiJumbo(emoji)
-            buf.push(buffer)
-          }
+      let buffer = await twemojiJumbo(args[0])
+      message.channel.send(buffer)
     }
-
-     mg(buf).then((img) => {
-       img.getBuffer("image/png", (err, buf) => {
-         if(err) {
-           msg.channel.send("something went wrong, perhaps try again later")
-         }
-         msg.channel.send(buf)
-       })
-     })
-    }
+  } else {
+    var buf = []
+  for (emoji of args) {
+    if(emoji.startsWith("<")) {
+          let id = emoji.split(":")[2].slice(0, -1)
+          let buffer = await discordJumbo(id, "png")
+          buf.push(buffer.buffer)
+        } else {
+          let buffer = await twemojiJumbo(emoji)
+          buf.push(buffer)
+        }
   }
+
+   mg(buf).then((img) => {
+     img.getBuffer("image/png", (err, buf) => {
+       if(err) {
+         message.channel.send(err)
+         throw err
+       }
+       message.channel.send(buf)
+     })
+   })
+  }
+}
 
 module.exports.help = {
   name: "jumbo"
